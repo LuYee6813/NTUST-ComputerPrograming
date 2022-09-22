c8e75b5b796833a7da5eaa2c41a125a7289525d5
LuYee6813 <aulaul920106@gmail.com>
2022-09-22 16:45:31 +0800

    homework

---
+++ PracticeHomework/C111/C1110919/C1110919Q01/main.c
@@ -0,0 +1,11 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{	
+	printf("  *\n");
+	printf(" ***\n");
+	printf("*****\n");
+	return 0;
+}
---
+++ PracticeHomework/C111/C1110919/C1110919Q02/main.c
@@ -0,0 +1,20 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{	
+	float n1,n2,ans;
+	scanf("%f",&n1);
+	scanf("%f", &n2);
+
+	ans = n1 + n2;
+	printf("%.2f + %.2f = %.2f\n", n1 , n2,ans);
+	ans = n1 - n2;
+	printf("%.2f - %.2f = %.2f\n", n1 , n2,ans);
+	ans = n1 * n2;
+	printf("%.2f * %.2f = %.2f\n", n1 , n2,ans);
+	ans = n1 / n2;
+	printf("%.2f / %.2f = %.2f\n", n1 , n2,ans);
+	return 0;
+}
