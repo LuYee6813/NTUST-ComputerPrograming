681ff374e6acd77faab1788c90a760a5a54b5152
LuYee6813 <aulaul920106@gmail.com>
2022-10-09 02:07:10 +0800

    update C110 MID01

---
+++ Midterm1_Exam/C110/Q03.c
@@ -0,0 +1,42 @@
+// 找重複的數字並相加
+#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+int qsort_cmp(const void* p1, const void* p2)
+{
+	int* a = (int*)p1;
+	int* b = (int*)p2;
+	return *a - *b;
+}
+int main()
+{
+	int arr[10];
+	for (int i = 0; i < 10; i++)
+		scanf("%d", &arr[i]);
+	qsort(arr, 10, sizeof(int), qsort_cmp);
+	int output[10], len = 0;
+	int temp = arr[0];
+	for (int i = 0; i < 9; i++)
+	{
+		if (temp == arr[i] && i != 0)
+			continue;
+		if (arr[i] == arr[i + 1])
+		{
+			output[len] = arr[i];
+			temp = arr[i];
+			len++;
+		}
+	}
+	if (len == 0)
+	{
+		puts("0");
+	}
+	else
+	{
+		int sum = 0;
+		for (int i = 0; i < len; i++)
+			sum += output[i];
+		printf("%d", sum);
+	}
+    return 0;
+}
