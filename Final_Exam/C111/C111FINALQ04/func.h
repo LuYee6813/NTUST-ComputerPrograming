6544c721897d106cbdf0070de74f7276a4b7598a
LuYee6813 <42244975+LuYee6813@users.noreply.github.com>
2022-12-30 21:17:16 +0800

    Update Final

--- Final_Exam/C111/C111FINALQ04/func.h
+++ Final_Exam/C111/C111FINALQ04/func.h
@@ -3,17 +3,17 @@
 
 // Implement the following functions.
 
-void parse(char *cases, double* a, double* b, double* c, double* d, double* e, double* f,double* g,double* h)
+void parse(char cases[], double* a, double* b, double* c, 
+    double* d, double* e, double* f, double* g, double* h)
 {
-    printf(cases);
-    scanf("%lf", &a);
-
+    sscanf(cases, "(%lf,%lf,%lf,%lf),(%lf,%lf,%lf,%lf)", a, b, c, d, e, f, g, h);
 }
 
-void calc_print(int i, double a, double b, double c, double d, double e, double f,double g,double h)
+void calc_print(int i, double a, double b, double c,
+    double d, double e, double f, double g, double h)
 {
     printf("Case %d:\n", i + 1);
-    printf("  Addition: (%.2lf,%.2lf,%.2lf,%2lf)\n", a + e, b + f, c + g, d + h);
-    printf("  Subtraction: (%.2lf,%.2lf,%.2lf,%2lf)\n", a - e, b - f, c - g, d - h);
-    printf("  Dot: %.2lf\n", a * e + b * f + c * g + d * h);
+    printf("  Addition: (%.2lf,%.2lf,%.2lf,%.2lf)\n", a + e, b + f , c + g, d + h);
+    printf("  Subtraction: (%.2lf,%.2lf,%.2lf,%.2lf)\n", a - e, b - f, c - g, d - h);
+    printf("  Dot: %.2lf\n", (a * e) + (b * f) + (c * g) + (d * h));
 }
